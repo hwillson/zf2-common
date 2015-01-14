474022c07354d5bcff4910e8c3dad963c8083121
hwillson <hugh@octonary.com>
2015-01-14 09:19:34 -0500

    Updated license.

--- modules/zf2_common/src/Zf2Common/View/Helper/IsReadOnly.php
+++ modules/zf2_common/src/Zf2Common/View/Helper/IsReadOnly.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * zf2-common
+ * https://github.com/hwillson/zf2-common
+ *
+ * @author     Hugh Willson, Octonary Inc.
+ * @copyright  Copyright (c)2015 Hugh Willson, Octonary Inc.
+ * @license    http://opensource.org/licenses/MIT
+ */
+
 namespace Zf2Common\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
